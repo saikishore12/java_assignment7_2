059f60e687796e3825d1f5315391afc0230676e7
Sai kishore Amirishetty <sai.kishi2010@gmail.com>
2016-09-25 21:17:24 +0530

    assignment

---
+++ src/findString.java
@@ -0,0 +1,14 @@
+
+public class findString {
+public static void main(String[] args) {
+	String text="hai this is Sai kishore Amirishetty";
+	String Search="is";
+	if(text.contains(Search)){
+		System.out.println("Substring is found");
+	}
+	else{
+		System.out.println("SubString is not found");
+	}
+	
+}
+}
